87bbe0752286387a301984bc4332e3bd50234974
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-17 18:24:27 +0900

    [delete]MonoBehaviour使わないので削除

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
@@ -7,17 +7,7 @@ public class WorldState
     public int Value;
 }
 
-public class WorldStates : MonoBehaviour
+public class WorldStates
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
