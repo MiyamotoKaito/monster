9aa6e7d3b9cb64d6fdd971f2ec4954722344bcfb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-17 19:00:58 +0900

    [add]シングルトン化

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/GWorld.cs
@@ -2,15 +2,5 @@ using UnityEngine;
 
 public class GWorld
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private static readonly GWorld instanse = new GWorld();
 }
