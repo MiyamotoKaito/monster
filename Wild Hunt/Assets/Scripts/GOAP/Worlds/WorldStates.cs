c07eabf5120bb4e5f3dc0c532331ceb947d5956f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-17 18:32:35 +0900

    [add]ステートを追加する関数

--- Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Worlds/WorldStates.cs
@@ -16,10 +16,22 @@ public class WorldStates
     {
         _states = new Dictionary<string, int>();
     }
-
+    /// <summary>
+    /// ステートを持っているか判別する
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public bool HasState(string key)
     {
         return _states.ContainsKey(key);
     }
-
+    /// <summary>
+    /// 引数に渡された値を辞書に登録する
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    private void AddState(string key, int value)
+    {
+        _states.Add(key, value);
+    }
 }
