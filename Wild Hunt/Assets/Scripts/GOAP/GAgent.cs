2edef14d965e4a03e5488eeb36e4c31190fe6972
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-23 12:38:15 +0900

    [add]アクション実行処理追加

--- Wild Hunt/Assets/Scripts/GOAP/GAgent.cs
+++ Wild Hunt/Assets/Scripts/GOAP/GAgent.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SubGoal
@@ -25,10 +26,11 @@ public class GAgent : MonoBehaviour
     //サブゴールの辞書
     private Dictionary<SubGoal, int> _subGoals = new Dictionary<SubGoal, int>();
 
-    //private GPlanner _gPlanner;
+    private GPlanner _gPlanner;
     private Queue<GAction> _actionQueue;
     private GAction _currentAction;
     private SubGoal _currentGoal;
+    private bool invoked;
     private void Awake()
     {
         //エージェントのアクションをすべて取得する
@@ -38,8 +40,47 @@ public class GAgent : MonoBehaviour
             _gActions.Add(a);
         }
     }
+    /// <summary>
+    /// アクションが実行され終わったら呼ぶ関数
+    /// </summary>
+    private void CompleteAction()
+    {
+        _currentAction._running = false;
+        _currentAction.PostPerform();
+        invoked = false;
+    }
     private void LateUpdate()
     {
-        
+        //現在のアクションが存在し、実行中であれば
+        if (_currentAction != null && _currentAction._running)
+        {
+            if (_currentAction._agent.hasPath && _currentAction._agent.remainingDistance < 1f)
+            {
+                if (!invoked)
+                {
+                    Invoke("CompleteAction", _currentAction._duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
+        //アクションキューが存在しているか、アクションが残っていれば
+        if (_gPlanner != null || _actionQueue != null)
+        {
+            _gPlanner = new GPlanner();
+
+            //優先度が高いサブゴールから降順にソートする
+            var sortedGoals = _subGoals.OrderByDescending(x => x.Value);
+            
+            foreach (var sg in sortedGoals)
+            {
+                _actionQueue = _gPlanner.Plan(_gActions, sg.Key.SubGoals, null);
+                if (_actionQueue != null)
+                {
+                    _currentGoal = sg.Key;
+                    break;
+                }
+            }
+        }
     }
 }
