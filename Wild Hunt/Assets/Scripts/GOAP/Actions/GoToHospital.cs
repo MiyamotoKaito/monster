e2c5eef9a29d13afd513cef8ece340a11ee09c91
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-11-28 09:18:05 +0900

    [add]病院に行くアクション

---
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/GoToHospital.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GoToHospital : GAction
+{
+    public override bool PrePerform()
+    {
+        Debug.Log("Going to the hospital...");
+        return true;
+    }
+    public override bool PostPerform()
+    {
+        Debug.Log("Arrived at the hospital.");
+        return true;
+    }
+}
