7ca23aefd562c649b166458dc432ae8f34302731
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-28 10:38:17 +0900

    [add]エージェントの信念を管理するクラス

---
+++ Wild Hunt/Assets/Scripts/GOAP/Beliefs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// エージェントの信念を管理するクラス
+/// </summary>
+public class Beliefs : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
