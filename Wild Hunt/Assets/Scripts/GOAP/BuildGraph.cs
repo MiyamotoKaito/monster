9e29e680b1ff652a6d8eaf7c2122bc6639a5fbff
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-11-29 18:59:44 +0900

    [add]グラフ制作用のスクリプト

---
+++ Wild Hunt/Assets/Scripts/GOAP/BuildGraph.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GOAP.BuildGraph
+{
+    public class BuildGraph
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
