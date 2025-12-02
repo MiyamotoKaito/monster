7885a90bc1dd3fdf3df788680a461d77c3ba9df4
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-02 19:48:41 +0900

    [add]インターフェースフォルダ

--- Wild Hunt/Assets/Scripts/GOAP/Interface/IAction.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Interface/IAction.cs
@@ -1,1 +1,17 @@
-﻿public interface IAction { }
+﻿using GOAP.WorldState;
+
+public interface IAction
+{
+    /// <summary>
+    /// 前提条件
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    bool PerCondition(WorldState state);
+    /// <summary>
+    /// 効果
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    bool Effect(WorldState state);
+}
