e03889dad3863c46f48279be797f789206c8a315
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-03 23:25:26 +0900

    [add]仮のワールドステート

---
+++ Wild Hunt/Assets/Scripts/GOAP/Interface/NewMonoBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NewMonoBehaviourScript : IWorldState
+{
+
+    [SerializeField]
+    private string _name;
+    [SerializeField]
+    private int _value;
+
+    public string Name => _name;
+
+    public int Value => _value;
+}
