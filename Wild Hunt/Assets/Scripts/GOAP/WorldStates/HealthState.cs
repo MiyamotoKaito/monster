ffc38bc342d4e3dc547482072e65d7e4388dd068
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-03 23:24:41 +0900

    [add]体力のワールドステートスクリプト

---
+++ Wild Hunt/Assets/Scripts/GOAP/WorldStates/HealthState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class HealthState : IWorldState
+{
+    public string Name => throw new System.NotImplementedException();
+
+    public int Value => throw new System.NotImplementedException();
+}
