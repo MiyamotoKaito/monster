f345b054be183430e11633535ec4f940941b12ce
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-04 01:46:25 +0900

    [add]Serializable属性付与

--- Wild Hunt/Assets/Scripts/GOAP/WorldStates/HungerState.cs
+++ Wild Hunt/Assets/Scripts/GOAP/WorldStates/HungerState.cs
@@ -6,4 +6,8 @@ public class Hunger : IWorldState
     public int Value => throw new System.NotImplementedException();
 
     public string Name => throw new System.NotImplementedException();
+    [SerializeField]
+    private string _name;
+    [SerializeField]
+    private int _value;
 }
