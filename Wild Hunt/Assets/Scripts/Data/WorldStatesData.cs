75420acf25ff4f52093f29a7f355d5a6af430f44
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-03 23:22:38 +0900

    [add]IWorldStateの配列を持つデータ

---
+++ Wild Hunt/Assets/Scripts/Data/WorldStatesData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Data/WorldState")]
+public class WorldStatesData : ScriptableObject
+{
+    public IWorldState[] WorldStates => _worldStates;
+
+    [SerializeReference, SubclassSelector]
+    private IWorldState[] _worldStates;
+}
