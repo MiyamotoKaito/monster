409a4cbc0cd954584829266075d50891398838ef
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-05 18:26:13 +0900

    [add]プランニング関数追加

--- Wild Hunt/Assets/Scripts/GOAP/GPlanner.cs
+++ Wild Hunt/Assets/Scripts/GOAP/GPlanner.cs
@@ -1,16 +1,41 @@
-﻿using UnityEngine;
-using GOAP.BuildGraph;
-public class GPlanner : MonoBehaviour
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace GOAP.GPlanner
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class GPlanner : MonoBehaviour
     {
-        
-    }
+        public Queue<IAction> Planning(List<IAction> actions,
+                                       Dictionary<string, int> goal,
+                                       Dictionary<string, int>currntState)
+        {
+            //利用可能なアクションのリスト
+            List<IAction> usableActions = new List<IAction>();
+            //利用可能なアクションをフィルタリングする
+            foreach (var action in actions)
+            {
+                //※修正
+                //アクションが現在のワールドステートで実行可能かの確認を行う処理を追加しないといけない
+                usableActions.Add(action);
+            }
+            List<GNode> leaves = new List<GNode>();
+            GNode start = new GNode(null, 0, currntState, null);
+        }
+        public bool BuildGraph(GNode parent,
+            List<GNode> leaves,
+            List<IAction> usebleAction,
+            Dictionary<string, int> goal)
+        {
+            bool foundPath = false;
+            foreach (var act in usebleAction)
+            {
+                if(act.PerCondition())
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        private void CanGoalAchieved()
+        {
+
+        }
     }
 }
