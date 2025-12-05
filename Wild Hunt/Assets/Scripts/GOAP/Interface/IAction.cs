1737564690718d040034c50b307d83e996afdd51
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-05 18:29:38 +0900

    [update]サマリー変更

--- Wild Hunt/Assets/Scripts/GOAP/Interface/IAction.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Interface/IAction.cs
@@ -3,13 +3,11 @@
     /// <summary>
     /// 前提条件
     /// </summary>
-    /// <param name="worldState"></param>
     /// <returns></returns>
     bool PerCondition(IWorldState worldState);
     /// <summary>
     /// 効果
     /// </summary>
-    /// <param name="worldState"></param>
     /// <returns></returns>
     bool Effect(IWorldState worldState);
 }
