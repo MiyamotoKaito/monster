faca5d324bf763392d9fe32072c04d66c8da98bf
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-05 18:33:02 +0900

    [add]GAgentクラス作成

---
+++ Wild Hunt/Assets/Scripts/GOAP/GAgent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GAgent : MonoBehaviour
+{
+    /// <summary>サブゴールと優先度の辞書<summary>
+    private Dictionary<GSubGoal, int> _subGoals = new();
+    /// <summary>ソートされたアクションのQueue<summary>
+    private Queue<IAction> _actionQueue = new();
+
+
+    [SerializeField, ReadOnly]
+    private IAction _currentAction;
+    [SerializeField, ReadOnly]
+    private GSubGoal _currentGoal;
+
+    private void 
+}
