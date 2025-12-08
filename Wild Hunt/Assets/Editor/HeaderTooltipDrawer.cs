865fcd6ed7fde0468e48dc466766db7b3aaf77c3
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-08 13:50:29 +0900

    [add]実際に書き込むクラス

---
+++ Wild Hunt/Assets/Editor/HeaderTooltipDrawer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(HeaderTooltipAttribute))]
+public class HeaderTooltipDrawer : PropertyDrawer
+{
+    private const float HEADERHEIGHT = 22f;
+
+    private const float SPACEBELOWHEADER = 2f;
+}
