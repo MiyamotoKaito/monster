4b2902abc8d8e5b2e946bb91f1b23d53b5324c24
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-08 14:14:55 +0900

    [add]処理実装

--- Wild Hunt/Assets/Editor/HeaderTooltipDrawer.cs
+++ Wild Hunt/Assets/Editor/HeaderTooltipDrawer.cs
@@ -12,4 +12,20 @@ public class HeaderTooltipDrawer : PropertyDrawer
     {
         return EditorGUI.GetPropertyHeight(property, label, true) + HEADERHEIGHT + SPACEBELOWHEADER;
     }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        HeaderTooltipAttribute att = (HeaderTooltipAttribute)attribute;
+
+        Rect headerRect = new Rect(position.x, position.y, position.width, HEADERHEIGHT);
+
+        Rect fieldRect = new Rect(position.x, position.y + HEADERHEIGHT + SPACEBELOWHEADER, 
+                                  position.width, EditorGUIUtility.singleLineHeight);
+
+        EditorGUI.LabelField(headerRect, att.Explanation, EditorStyles.boldLabel);
+
+        GUIContent content = new GUIContent(label.text, att.Explanation);
+
+        EditorGUI.PropertyField(fieldRect, property, content, true);
+    }
 }
