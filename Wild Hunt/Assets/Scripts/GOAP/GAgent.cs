49b31c46ec67c8f551923067e12d71ea78e721b9
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-08 21:33:48 +0900

    [add]アクションデータの変数とスタート内での初期化処理

--- Wild Hunt/Assets/Scripts/GOAP/GAgent.cs
+++ Wild Hunt/Assets/Scripts/GOAP/GAgent.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using GOAP.WorldStates;
+using GOAP.GPlanner;
 
 public class GAgent : MonoBehaviour
 {
@@ -8,11 +10,27 @@ public class GAgent : MonoBehaviour
     /// <summary>ソートされたアクションのQueue<summary>
     private Queue<IAction> _actionQueue = new();
 
+    [SerializeField]
+    [Header("アクションのデータ")]
+    private MonsterActionsData _actionsData;
 
     [SerializeField, ReadOnly]
     private IAction _currentAction;
     [SerializeField, ReadOnly]
     private GSubGoal _currentGoal;
 
-    private void 
+
+    private GPlanner _planner;
+    private WorldStates _worldStates;
+
+    private void Start()
+    {
+        _planner = GetComponent<GPlanner>();
+        _worldStates = WorldStates.Instance;
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
 }
