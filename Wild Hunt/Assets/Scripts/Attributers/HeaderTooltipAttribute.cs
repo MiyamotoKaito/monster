1729412c37daad0809b47e45bc89113ac6ab8d8c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-08 13:50:06 +0900

    [add]処理実装

--- Wild Hunt/Assets/Scripts/Attributers/HeaderTooltipAttribute.cs
+++ Wild Hunt/Assets/Scripts/Attributers/HeaderTooltipAttribute.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// HeaderとTooltipの機能を両方とも兼ね備えたカスタム属性クラス
+/// </summary>
 public class HeaderTooltipAttribute : PropertyAttribute
 {
+    //説明
+    public readonly string Explanation;
 
+    public HeaderTooltipAttribute(string explanation)
+    {
+        Explanation = explanation;
+    }
 }
