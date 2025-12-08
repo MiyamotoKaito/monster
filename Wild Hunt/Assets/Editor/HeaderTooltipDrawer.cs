36d92858651b35bf1f0ad4ee5c9ab9693fee24b0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-08 13:53:32 +0900

    [add]プロパティの描画に必要な全体の高さを計算する処理関数追加

--- Wild Hunt/Assets/Editor/HeaderTooltipDrawer.cs
+++ Wild Hunt/Assets/Editor/HeaderTooltipDrawer.cs
@@ -7,4 +7,9 @@ public class HeaderTooltipDrawer : PropertyDrawer
     private const float HEADERHEIGHT = 22f;
 
     private const float SPACEBELOWHEADER = 2f;
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true) + HEADERHEIGHT + SPACEBELOWHEADER;
+    }
 }
