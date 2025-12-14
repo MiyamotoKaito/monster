d4c5a557289b6743f5ae281d03a9356508cd2848
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-14 23:17:09 +0900

    [add]ターゲットに移動するためのアクション

---
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/MoveToTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : IAction
+{
+    public Dictionary<string, int> Preconditions => throw new System.NotImplementedException();
+
+    public Dictionary<string, int> Effects => throw new System.NotImplementedException();
+
+    public int Cost => throw new System.NotImplementedException();
+
+
+    public bool CheckPrecondition(GAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Execute(GAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Perform(GAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+}
