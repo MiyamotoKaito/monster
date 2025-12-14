9ad57b4e330e748a5f08245b965051d3c9df0125
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-14 23:44:56 +0900

    [add]関数の中身追加

--- Wild Hunt/Assets/Scripts/GOAP/Actions/MoveToTarget.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/MoveToTarget.cs
@@ -3,25 +3,56 @@ using UnityEngine;
 
 public class MoveToTarget : IAction
 {
-    public Dictionary<string, int> Preconditions => throw new System.NotImplementedException();
+    public Dictionary<string, int> Preconditions => new Dictionary<string, int>() { {"HasTarget", 1 } };
 
-    public Dictionary<string, int> Effects => throw new System.NotImplementedException();
+    public Dictionary<string, int> Effects => new Dictionary<string, int>() { { "AtTarget", 1 } };
 
-    public int Cost => throw new System.NotImplementedException();
+    public int Cost => _cost;
 
+    [SerializeField]
+    private int _cost;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _stopDistance;
+    
+    //実行時変数
+    private Transform _target;
+    private bool _isMoving;
+    private string _targetTag;
 
     public bool CheckPrecondition(GAgent agent)
     {
-        throw new System.NotImplementedException();
+        return _target != null;
     }
 
     public void Execute(GAgent agent)
     {
-        throw new System.NotImplementedException();
+        GameObject targetobj = GameObject.FindGameObjectWithTag(_targetTag);
+        if (targetobj != null)
+        {
+            _target = targetobj.transform;
+            _isMoving = true;
+        }
+
+        _isMoving = false;
     }
 
     public bool Perform(GAgent agent)
     {
-        throw new System.NotImplementedException();
+        if (!_isMoving || _target == null) return false;
+
+        float distance = Vector3.Distance(agent.transform.position, _target.position);
+        if (distance <= _stopDistance)
+        {
+            Debug.Log($"{GetType().Name}" + "アクション完了");
+            return true;
+        }
+        //移動
+        Vector3 direction = (_target.position - agent.transform.position).normalized;
+        agent.transform.position += direction * _speed * Time.deltaTime;
+
+        //まだ完了していない
+        return false;
     }
 }
