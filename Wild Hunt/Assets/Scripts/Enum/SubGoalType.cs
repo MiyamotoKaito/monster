e0642648a0a3fd2148be4093edfe60644c0d709f
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-17 16:33:11 +0900

    [add]サブゴールのEnum

---
+++ Wild Hunt/Assets/Scripts/Enum/SubGoalType.cs
@@ -0,0 +1,15 @@
+﻿public enum SubGoalType
+{
+    //Conditions
+    /// <summary>おなかいっぱい</summary>
+    IsFull,
+    /// <summary>喉が潤った</summary>
+    IsThroatFeelsBetter,
+    /// <summary>疲れが取れた</summary>
+    IsRefreshed,
+
+
+    //Movement
+    /// <summary>攻撃を終えた</summary>
+    IsAttacked,
+}
