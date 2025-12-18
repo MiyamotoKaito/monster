b41853b19a9435dd7cf83b91b8b4ab7b2518b41d
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-18 23:23:48 +0900

    [update]Exuecuteメソッドの処理内容変更

--- Wild Hunt/Assets/Scripts/GOAP/Actions/Eat.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/Eat.cs
@@ -1,45 +1,39 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Eat : IAction
+public class Eat : ActionBase, IAction
 {
     public Dictionary<string, int> Preconditions => new Dictionary<string, int>() { { _precondition.ToString(), 1 } };
 
-    public Dictionary<string, int> Effects => new Dictionary<string, int>() { { _subGoal.ToString(), 10 }, { "HasTarget", 0 } };
+    public Dictionary<string, int> Effects => new Dictionary<string, int>() { { _subGoal.ToString(), 1 }, { "HasTarget", 0 }, {"AtTarget", 0 } };
 
     public int Cost => _cost;
 
-    [SerializeField]
-    private int _cost;
-    [SerializeField]
-    private string _targetTag;
-    [SerializeField]
-    private WorldStateType _precondition;
-    [SerializeField]
-    private SubGoalType _subGoal;
-    private Transform _target;
+    [SerializeField] private string _targetTag;
+    [SerializeField] private WorldStateType _precondition;
+    [SerializeField] private SubGoalType _subGoal;
 
+    private Transform _target;
     private Dictionary<string, int> _preconditions;
     private Dictionary<string, int> _effects;
-
-    private bool _eat = false;
     public bool CheckPrecondition(GAgent agent)
     {
         return _target != null;
     }
-
+    public void Execute(GAgent agent)
+    {
+        _target = agent.TargetObj.transform;
+    }
     public bool Perform(GAgent agent)
     {
         // ターゲットが消滅していたら失敗
         if (_target == null) return false;
 
-        Debug.Log("EAT: 食べました");
-
         // SurvivalStatsに通知して空腹タイマーをリセット
         var survivalStats = agent.GetComponent<SurvivalStats>();
         if (survivalStats != null)
         {
-            survivalStats.OnFed();
+            survivalStats.OnAte();
         }
         else
         {
@@ -48,21 +42,6 @@ public class Eat : IAction
 
         // 食べ物を消去
         GameObject.Destroy(_target.gameObject);
-
         return true; // 完了
-    }
-
-    public void Execute(GAgent agent)
-    {
-        GameObject targetobj = GameObject.FindGameObjectWithTag(_targetTag);
-        if (targetobj != null)
-        {
-            _target = targetobj.transform;
-            _eat = true;
-        }
-        else
-        {
-            _eat = false;
-        }
     }
 }
