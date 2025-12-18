1c22f69b5c0b6cea5627fb5b288f9f2323e3487a
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-18 23:18:58 +0900

    [add]飲むアクション

---
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/Drink.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink : ActionBase, IAction
+{
+    public Dictionary<string, int> Preconditions => new Dictionary<string, int>() { { _precondition.ToString(), 1 } };
+
+    public Dictionary<string, int> Effects => new Dictionary<string, int>() { { _subGoal.ToString(), 1 } };
+
+    public int Cost => _cost;
+
+
+    [SerializeField] private string _targetTag;
+    [SerializeField] private float _drinkAmount;
+    [SerializeField] private WorldStateType _precondition;
+    [SerializeField] private SubGoalType _subGoal;
+
+    private Transform _target;
+    //private Dictionary<string, int> _conditions = new Dictionary<string, int>() { { _precondition.ToString() } } 
+    //private Dictionary<string, int>_effect = new Dictionary<string, int>();
+
+    public bool CheckPrecondition(GAgent agent)
+    {
+        if (!_target) return false;
+
+        return true;
+    }
+
+    public void Execute(GAgent agent)
+    {
+        _target = agent.TargetObj.transform;
+
+        if (_target == null)
+        {
+            Debug.Log("<color=red>飲み物のターゲットが見つかりませんでした</color>");
+        }
+    }
+
+    public bool Perform(GAgent agent)
+    {
+        if(!_target) return false;
+
+        var survival = agent.GetComponent<SurvivalStats>();
+        if (survival != null)
+        {
+            survival.OnDrank();
+        }
+        else
+        {
+            Debug.LogWarning("Drink: SurvivalStatsコンポーネントが見つかりません");
+        }
+
+        GameObject.Destroy(_target.gameObject);
+        return true;
+    }
+}
