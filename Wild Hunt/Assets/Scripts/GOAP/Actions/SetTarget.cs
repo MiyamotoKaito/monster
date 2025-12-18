d1fb2e78dc8387018d04bffe28d46a448cceb33f
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-18 23:20:58 +0900

    [add]アクションベース継承

--- Wild Hunt/Assets/Scripts/GOAP/Actions/SetTarget.cs
+++ Wild Hunt/Assets/Scripts/GOAP/Actions/SetTarget.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class SetTarget : IAction
+public class SetTarget : ActionBase,IAction
 {
     // 前提条件を空にする（ターゲットがいてもいなくても、探し直せるようにする）
     public Dictionary<string, int> Preconditions => new Dictionary<string, int>();
 
     public Dictionary<string, int> Effects => new Dictionary<string, int>() { { _effect.ToString(), 1 } };
     public int Cost => _cost;
-    [SerializeField] private int _cost = 1;
     [SerializeField] private WorldStateType _effect;
+    private Transform _target;
 
     public bool CheckPrecondition(GAgent agent) => true; // 常に実行可能にする
 
@@ -17,9 +17,8 @@ public class SetTarget : IAction
 
     public bool Perform(GAgent agent)
     {
-        // 食べ物があるかチェック
-        var targetObj = GameObject.FindGameObjectWithTag("Food");
-        if (targetObj == null) return false;
+        _target = agent.TargetObj.transform;
+        if (_target == null) return false;
 
         // 【重要】実行時にワールドステートを更新して、次のMoveアクションを動かせるようにする
         GOAP.WorldStates.WorldStates.Instance.ModifyState(_effect.ToString(), 1);
